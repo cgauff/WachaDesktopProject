b1ab07606d1e469a17a42754a03f5daad7b585fc
cgauff <colby.gauff@gmail.com>
2017-08-13 23:14:16 -0500

    Testing Gui

---
+++ Software/WachaProject/WachaProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WachaProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Software/WachaSoftwareDevelopemnt/WachaSoftwareDevelopemnt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WachaSoftwareDevelopemnt
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
