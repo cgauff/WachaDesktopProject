a60ee0da1f3040b1b49c59e9b2fafb024e70b2df
Colby Gauf <colby.gauff@gmail.com>
2017-08-14 00:08:45 -0500

    Outline
    User gui out line

---
+++ WachaBusinessSoftware/WachaBusinessSoftware/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace WachaBusinessSoftware
+{
+    partial class MainHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerListingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customerOrdersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tachtionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exisitingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.workOrderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openOrdersToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.serviceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.servicesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuBar
+            // 
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.customerToolStripMenuItem,
+            this.tachtionToolStripMenuItem,
+            this.workOrderToolStripMenuItem,
+            this.serviceToolStripMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(853, 24);
+            this.menuBar.TabIndex = 0;
+            this.menuBar.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // customerToolStripMenuItem
+            // 
+            this.customerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem1,
+            this.customerListingToolStripMenuItem,
+            this.customerOrdersToolStripMenuItem});
+            this.customerToolStripMenuItem.Name = "customerToolStripMenuItem";
+            this.customerToolStripMenuItem.Size = new System.Drawing.Size(71, 20);
+            this.customerToolStripMenuItem.Text = "Customer";
+            // 
+            // newToolStripMenuItem1
+            // 
+            this.newToolStripMenuItem1.Name = "newToolStripMenuItem1";
+            this.newToolStripMenuItem1.Size = new System.Drawing.Size(164, 22);
+            this.newToolStripMenuItem1.Text = "New";
+            // 
+            // customerListingToolStripMenuItem
+            // 
+            this.customerListingToolStripMenuItem.Name = "customerListingToolStripMenuItem";
+            this.customerListingToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.customerListingToolStripMenuItem.Text = "Customer Listing";
+            // 
+            // customerOrdersToolStripMenuItem
+            // 
+            this.customerOrdersToolStripMenuItem.Name = "customerOrdersToolStripMenuItem";
+            this.customerOrdersToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.customerOrdersToolStripMenuItem.Text = "Customer Orders";
+            // 
+            // tachtionToolStripMenuItem
+            // 
+            this.tachtionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.vewToolStripMenuItem,
+            this.exisitingToolStripMenuItem});
+            this.tachtionToolStripMenuItem.Name = "tachtionToolStripMenuItem";
+            this.tachtionToolStripMenuItem.Size = new System.Drawing.Size(77, 20);
+            this.tachtionToolStripMenuItem.Text = "Technician";
+            // 
+            // vewToolStripMenuItem
+            // 
+            this.vewToolStripMenuItem.Name = "vewToolStripMenuItem";
+            this.vewToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.vewToolStripMenuItem.Text = "New";
+            // 
+            // exisitingToolStripMenuItem
+            // 
+            this.exisitingToolStripMenuItem.Name = "exisitingToolStripMenuItem";
+            this.exisitingToolStripMenuItem.Size = new System.Drawing.Size(120, 22);
+            this.exisitingToolStripMenuItem.Text = "Exisiting ";
+            // 
+            // workOrderToolStripMenuItem
+            // 
+            this.workOrderToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openOrdersToolStripMenuItem,
+            this.aToolStripMenuItem});
+            this.workOrderToolStripMenuItem.Name = "workOrderToolStripMenuItem";
+            this.workOrderToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.workOrderToolStripMenuItem.Text = "Work Order";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.newToolStripMenuItem.Text = "New Order";
+            // 
+            // openOrdersToolStripMenuItem
+            // 
+            this.openOrdersToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.openOrdersToolStripMenuItem.Name = "openOrdersToolStripMenuItem";
+            this.openOrdersToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.openOrdersToolStripMenuItem.Text = "Active Orders";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.viewToolStripMenuItem.Text = "View ";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.updateToolStripMenuItem.Text = "Update";
+            // 
+            // aToolStripMenuItem
+            // 
+            this.aToolStripMenuItem.Name = "aToolStripMenuItem";
+            this.aToolStripMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.aToolStripMenuItem.Text = "Archived Orders";
+            // 
+            // serviceToolStripMenuItem
+            // 
+            this.serviceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.servicesToolStripMenuItem,
+            this.rateToolStripMenuItem});
+            this.serviceToolStripMenuItem.Name = "serviceToolStripMenuItem";
+            this.serviceToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.serviceToolStripMenuItem.Text = "Tools";
+            // 
+            // servicesToolStripMenuItem
+            // 
+            this.servicesToolStripMenuItem.Name = "servicesToolStripMenuItem";
+            this.servicesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.servicesToolStripMenuItem.Text = "Services";
+            // 
+            // rateToolStripMenuItem
+            // 
+            this.rateToolStripMenuItem.Name = "rateToolStripMenuItem";
+            this.rateToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rateToolStripMenuItem.Text = "Rate";
+            // 
+            // MainHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 478);
+            this.Controls.Add(this.menuBar);
+            this.MainMenuStrip = this.menuBar;
+            this.Name = "MainHome";
+            this.Text = "WachaHome";
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuBar;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem customerListingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customerOrdersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tachtionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exisitingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem workOrderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openOrdersToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem serviceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem servicesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rateToolStripMenuItem;
+    }
+}
+
